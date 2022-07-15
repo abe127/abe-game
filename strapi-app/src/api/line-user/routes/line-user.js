eafc05255d3aff1b977ee485e60e26521f9a2df7
阿部ひかり <>
2022-07-15 11:38:33 +0900

    lineUser作成

---
+++ strapi-app/src/api/line-user/routes/line-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * line-user router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::line-user.line-user');
