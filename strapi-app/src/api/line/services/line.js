2a9b75c75ff5eabacc78fdff17166706f83d4bfb
laut0104 <78589722+laut0104@users.noreply.github.com>
2022-07-19 22:55:04 +0900

    名前登録機能追加

--- strapi-app/src/api/line/services/line.js
+++ strapi-app/src/api/line/services/line.js
@@ -15,16 +15,15 @@ module.exports = {
 
     try {
       const data = {
-        data: { user_id: event.source.userId },
+        data: { user_id: event.source.userId , context: 'name_setting', },
       };
       const entity = await strapi.entityService.create(
         "api::line-user.line-user",
         data
       );
-
       const message = {
         type: "text",
-        text: "友達登録ありがとうございます！",
+        text: "友達登録ありがとうございます！\nゲームで使用する名前を送ってください。\n※10文字以内、11文字以上は切り捨て",
       };
       client.replyMessage(event.replyToken, message);
     } catch (error) {
@@ -48,4 +47,34 @@ module.exports = {
       strapi.log.error(error);
     }
   },
+
+
+  async messagingEvent(event) {
+    if (!event?.source?.userId) return;
+    try {
+      const context = await strapi
+                  .query("api::line-user.line-user")
+                  .findOne({select: ['context'], where: { user_id: event.source.userId }});
+      if(context.context === 'name_setting'){
+        if (event.message.text.length >= 11) {
+          var display_name = event.message.text.substr( 0, 10 );
+        }
+        else{
+          var display_name = event.message.text;
+        }
+        console.log(display_name);
+        await strapi
+          .query("api::line-user.line-user")
+          .update({ where: { user_id: event.source.userId } , data: {display_name: display_name, context: 'done'}});
+        const message = {
+          type: "text",
+          text: `${display_name}　に設定しました！`,
+        };
+        client.replyMessage(event.replyToken, message);
+      }
+
+    } catch (error) {
+      strapi.log.error(error);
+    }
+  }
 };
